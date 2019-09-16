36e9e9e33b43cdf66cc332c48e5f7c218fd2c252
Pedro <p.paiva.pdo@gmail.com>
2019-09-16 19:39:25 -0300

    commit classes

---
+++ src/A.java
@@ -0,0 +1,27 @@
+
+public class A {
+	int A1;
+	float A2;
+	
+	public void setA1(int A1){
+		this.A1 = A1;
+	}
+	public int getA1() {
+		return A1;
+	}
+	
+	public void setA2(int A2){
+		this.A2 = A2;
+	}
+	
+	public float getA2() {
+		return A2;
+	}
+	
+	public void MA1() {
+		System.out.println("Metodo MA1");
+	}
+	public void MA2() {
+		System.out.println("Metodo MA2");
+	}
+}
---
+++ src/B.java
@@ -0,0 +1,27 @@
+
+public class B {
+	int B1;
+	float B2;
+	
+	public void setB1(int B1){
+		this.B1 = B1;
+	}
+	public int getB1() {
+		return B1;
+	}
+	
+	public void setB2(int B2){
+		this.B2 = B2;
+	}
+	
+	public float getB2() {
+		return B2;
+	}
+	
+	public void MB1() {
+		System.out.println("Metodo MB1");
+	}
+	public void MB2() {
+		System.out.println("Metodo MB2");
+	}
+}
